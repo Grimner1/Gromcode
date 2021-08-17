f4c0a71052be2b1490592370114324bb30298c20
Grimner1 <ursuld@gmail.com>
2021-08-17 13:32:17 +0300

    add LC 5

---
+++ live-coding/5/index.js
@@ -0,0 +1,35 @@
+
+
+
+
+const sum = (num1, 2) => num1 + num2;
+
+console.log(sum(3));
+console.log(sum(0, -5));
+
+--------------
+
+const getMessage = (5) => 
+    'I am ' + number + ' years old';
+
+console.log(getMessage());
+console.log(getMessage());
+----------------
+
+const mult = (firstNum, secondNum) => firstNum * secondNum;
+
+console.log(mult(2, 8));
+console.log(mult(-4, -10));
+
+-------------------
+
+const square = number => number * number;
+
+console.log(square(4));
+console.log(square(-8));
+
+-------------------------
+
+const getMagicNumber = () => 17;
+
+console.log(getMagicNumber());
