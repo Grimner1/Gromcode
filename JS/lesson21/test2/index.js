a1c44cf15669a1803c12816f8f71bdd3f6ba89bf
Grimner1 <ursuld@gmail.com>
2021-09-07 22:28:33 +0300

    21

--- JS/lesson21/test2/index.js
+++ JS/lesson21/test2/index.js
@@ -1,11 +1,11 @@
-export function getItemList() {
+export function getItemsList() {
   let elementsList = document.querySelectorAll('.technology');
   console.dir(elementsList);
   return elementsList;
 }
 console.log(getItemList());
 
-export function getItemArray() {
+export function getItemsArray() {
   let elements = document.querySelectorAll('.tool');
   //   let elementsArray = [...elements];
   console.dir(Array.from(elements));
