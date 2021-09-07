d041ae638fe330c13809e4ca237d18721a67eecb
Grimner1 <ursuld@gmail.com>
2021-09-07 18:54:41 +0300

    add l21 t1

---
+++ JS/lesson21/test1/index.js
@@ -0,0 +1,11 @@
+export function getTitleElement() {
+  let titileElement = document.querySelector('.title');
+  console.dir(titileElement);
+  return titileElement;
+}
+
+export function getInputElement() {
+  let inputElement = document.querySelector('input[type = "text"]');
+  console.dir(inputElement);
+  return inputElement;
+}
