8019aecaf1c65d2d2f0318acbc40ce1e8d6d97f9
Grimner1 <ursuld@gmail.com>
2021-09-11 00:22:24 +0300

    7 2

--- JS/lesson22/test7/index.js
+++ JS/lesson22/test7/index.js
@@ -1,4 +1,4 @@
-const btn = document.querySelectorAll('.btn');
+export const btn = document.querySelectorAll('.btn');
 // console.log(...btn);
 const body = document.querySelector('body');
 // console.log(body);
