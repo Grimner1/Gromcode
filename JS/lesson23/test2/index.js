fa312ccf3ed8bf492ff79f469f048180f11f5128
Grimner1 <ursuld@gmail.com>
2021-09-13 19:33:00 +0300

    add l23 t2

---
+++ JS/lesson23/test2/index.js
@@ -0,0 +1,33 @@
+//  - метод click назначаем на .arena
+//  - делаем проверку на (td) и возвращаем его (id)
+//  - по этому (td) находим его родителя (tr) и возвращаем его (id)
+//  - по этому (tr) находим его родителя (table) и возвращаем его (id)
+
+const boardSpan = document.querySelector('.board__selected-seat');
+const arena = document.querySelector('.arena');
+const seat = document.querySelector('.sector__seat');
+
+const findSeat = (event) => {
+  let seat;
+  let line;
+  let sector;
+  if (event.target.tagName !== 'TD') {
+    return;
+  } else {
+    seat = event.target.dataset.id;
+    // console.log(seat);
+
+    let lineOfSeat = event.target.closest('tr');
+    line = lineOfSeat.dataset.id;
+    // console.log(line);
+
+    let sectorOfLine = lineOfSeat.closest('table');
+    sector = sectorOfLine.dataset.id;
+    // console.log(sector);
+
+    boardSpan.textContent = `S ${sector} - L ${line} - S ${seat}`;
+  }
+};
+
+arena.addEventListener('click', findSeat);
+// console.log(seat);
