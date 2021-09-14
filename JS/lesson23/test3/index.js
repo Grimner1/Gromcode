14aa0e8634ea1bf5810e840a752333c03d44e43a
Grimner1 <ursuld@gmail.com>
2021-09-14 18:21:53 +0300

    1

--- JS/lesson23/test3/index.js
+++ JS/lesson23/test3/index.js
@@ -52,80 +52,88 @@ let taskId = 6;
 const pressToAddTask = () => {
   if (!fieldinput.value) {
     return;
-  } else {
-    const textOfInput = fieldinput.value;
-    const newTask = { text: textOfInput, done: false, id: taskId };
-    taskId += 1;
-    tasks.push(newTask);
-    listElem.innerHTML = '';
-
-    renderTasks(tasks);
   }
+  const textOfInput = fieldinput.value;
+  const newTask = { text: textOfInput, done: false, id: taskId };
+  taskId += 1;
+  tasks.push(newTask);
+  listElem.innerHTML = '';
+
+  renderTasks(tasks);
 };
 
 creatButton.addEventListener('click', pressToAddTask);
 
 // -------------------------------------------
 
-// const funcDuneCheckbox = (event) => {
-//   // console.log(event.target.closest('li'));
-//   const liElement = event.target.closest('li');
-//   const checkboxElement = event.target;
-//   // console.log(liElement);
-//   //   console.log(checkboxElement.dataset.id);
-//   //   console.log(tasks[checkboxElement.dataset.id].done);
-//   //   console.log(checkboxElement.checked);
-//   const findEl = tasks.filter(({ id }) => {
-//     // console.log(id);
-//     // console.log(checkboxElement.dataset.id == id);
-//     if (id == checkboxElement.dataset.id) {
-//       return id;
-//     }
-//   });
-//   // console.log(findEl[0].done);
-//   if (checkboxElement.checked) {
-//     liElement.classList.add('list__item_done');
-//     // console.log(checkboxElement.dataset.id);
-//     findEl[0].done = true;
-//     // tasks[checkboxElement.dataset.id].done = true;
-//     tasks;
-//   } else {
-//     liElement.classList.remove('list__item_done');
-//     // console.log(checkboxElement.dataset.id);
-//     findEl[0].done = false;
-//     // tasks[checkboxElement.dataset.id].done = false;
-//   }
-//   listElem.innerHTML = '';
-//   renderTasks(tasks);
-// };
-
-// listElem.addEventListener('change', funcDuneCheckbox);
-
-// ------------------------------------------так и не понял как тут через map -----
-
-const func = (event) => {
+const funcDuneCheckbox = (event) => {
+  // console.log(event.target.closest('li'));
   const liElement = event.target.closest('li');
   const checkboxElement = event.target;
-  tasks.map(({ id }, index) => {
+  // console.log(liElement);
+  //   console.log(checkboxElement.dataset.id);
+  //   console.log(tasks[checkboxElement.dataset.id].done);
+  //   console.log(checkboxElement.checked);
+  const findEl = tasks.filter(({ id }) => {
+    // console.log(id);
+    // console.log(checkboxElement.dataset.id == id);
     if (id == checkboxElement.dataset.id) {
-      if (checkboxElement.checked) {
-        console.log(tasks);
-        liElement.classList.add('list__item_done');
-        tasks[index].done = true;
-      } else {
-        console.log(tasks);
-        liElement.classList.remove('list__item_done');
-        tasks[index].done = false;
-      }
+      return id;
     }
   });
-};
-
-const funcDuneCheckbox = (event) => {
-  func(event);
-
+  // console.log(findEl[0].done);
+  if (checkboxElement.checked) {
+    liElement.classList.add('list__item_done');
+    // console.log(checkboxElement.dataset.id);
+    findEl[0].done = checkboxElement.checked;
+    // tasks[checkboxElement.dataset.id].done = true;
+    tasks;
+  } else {
+    liElement.classList.remove('list__item_done');
+    // console.log(checkboxElement.dataset.id);
+    findEl[0].done = checkboxElement.checked;
+    // tasks[checkboxElement.dataset.id].done = false;
+  }
   listElem.innerHTML = '';
   renderTasks(tasks);
 };
 
 listElem.addEventListener('change', funcDuneCheckbox);
+
+// ------------------------------------------так и не понял как тут через map -----
+
+// const func = (event) => {
+//   const liElement = event.target.closest('li');
+//   const checkboxElement = event.target;
+//   const x = tasks.map((element) => {
+//     console.log(element.id);
+//     if (element.id == checkboxElement.dataset.id) {
+//       return {
+//         ...element,
+//         done: checkboxElement.checked,
+//       };
+
+//       // if (checkboxElement.checked) {
+//       //   // console.log(tasks);
+//       //   liElement.classList.add('list__item_done');
+//       //   tasks[index].done = true;
+//       // } else {
+//       //   console.log(tasks);
+//       //   liElement.classList.remove('list__item_done');
+//       //   tasks[index].done = false;
+//       // }
+//     }
+//     return element;
+//   });
+//   console.log(x);
+//   return x;
+// };
+
+// const funcDuneCheckbox = (event) => {
+//   func(event);
+
+//   listElem.innerHTML = '';
+//   renderTasks(func);
+// };
+
+// listElem.addEventListener('change', funcDuneCheckbox);
