e3f766b56c4d4393b295c662a9a7b190f83ca8e2
Grimner1 <ursuld@gmail.com>
2021-09-17 10:15:03 +0300

    add l27 t3

---
+++ JS/lesson27/test3/scripts/index.js
@@ -0,0 +1,15 @@
+import { initTodoListHandlers } from './todoList.js';
+import { renderTask } from './renderer.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  renderTask();
+  initTodoListHandlers();
+});
+
+const onStorageChange = (e) => {
+  if (e.key === 'taskList') {
+    renderTask();
+  }
+};
+
+window.addEventListener('storage', onStorageChange);
