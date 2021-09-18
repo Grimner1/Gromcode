33a04d55b1c6272428401450032ca6098751fcab
Grimner1 <ursuld@gmail.com>
2021-09-18 19:27:42 +0300

    addl30 t1

---
+++ JS/lesson30/test1/index.js
@@ -0,0 +1,40 @@
+export const addImageWithPromise = (imgSrc) => {
+  const p = new Promise((resolve, reject) => {
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'somePhoto');
+    imgElem.src = imgSrc;
+    const imgContainer = document.querySelector('.page');
+    imgContainer.append(imgElem);
+
+    const onImageLoaded = () => {
+      resolve(imgElem);
+    };
+
+    imgElem.addEventListener('load', onImageLoaded);
+
+    imgElem.addEventListener('error', () => reject(new Error('Image load is failed')));
+  });
+  return p;
+};
+
+const imgSrc =
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
+
+const result = addImageWithPromise(imgSrc);
+
+const onImageLoaded = (imgElem) => {
+  const { width, height } = imgElem;
+  const sizeElem = document.querySelector('.image-size');
+  sizeElem.textContent = `${width} x ${height}`;
+};
+
+result.then((some) => console.log(some));
+result.then((some) => onImageLoaded(some));
+// result.then((some) => onImageLoaded(some));
+result.catch((error) => console.log(error));
+
+// examples
+// addImage(
+//   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+//   onImageLoaded,
+// );
