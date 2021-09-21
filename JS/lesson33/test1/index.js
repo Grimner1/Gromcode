5613ee17b4391827e16343b161bb536898468880
Grimner1 <ursuld@gmail.com>
2021-09-21 09:50:33 +0300

    add l33 t1

---
+++ JS/lesson33/test1/index.js
@@ -0,0 +1,24 @@
+const input = document.querySelector('.name-form__input');
+const button = document.querySelector('.name-form__btn');
+
+const userAvatar = document.querySelector('.user__avatar');
+const userName = document.querySelector('.user__name');
+const userLocation = document.querySelector('.user__location');
+
+const url = 'https://api.github.com/users/';
+
+const fetchUserData = (userName) => {
+  console.log(`${url}${userName}`);
+  return fetch(`${url}${userName}`).then((response) => response.json());
+};
+
+const getUserData = (userData) => {
+  console.log(userData);
+};
+
+const onSearch = () => {
+  const searchText = input.value;
+  fetchUserData(searchText).then((downloadData) => getUserData(downloadData));
+};
+
+button.addEventListener('click', onSearch);
