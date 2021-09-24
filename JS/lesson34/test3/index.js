b10acbcd41e496747723246c7ecc593aefafa437
Grimner1 <ursuld@gmail.com>
2021-09-24 20:32:44 +0300

    add l34 t3

---
+++ JS/lesson34/test3/index.js
@@ -0,0 +1,48 @@
+//  - по нажатию .submit-button - отправка данных формы на сервер:
+//      - собрать данные в объект из: id: email, name, password;
+//          или попробовать собрать с массива классов: .form-input
+//      - объект отправить на сервер;
+//      - объект вывести в алерт;
+//      - очистить поля форы;
+//  - Если произошло ошибка при создании пользователя - вывести
+//       'Failed to create user' в .error-text;
+//          - если было изменение в одном из полей - ошибку убрать;
+
+const loginForm = document.querySelector('.login-form');
+const inputArray = document.querySelectorAll('.form-input');
+const submitButton = document.querySelector('.submit-button');
+const emailField = document.querySelector('#email');
+const nameField = document.querySelector('#name');
+const passField = document.querySelector('#password');
+
+const url = 'https://614c751d59e92d00176ad25f.mockapi.io/tasksList/user';
+
+const setInfo = (userInfo) =>
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(userInfo),
+  });
+
+const onButtonClick = () => {
+  const userInfo = {
+    email: emailField.value,
+    name: nameField.value,
+    password: passField.value,
+  };
+  const response = setInfo(userInfo).then((el) => el);
+  alert(response); // <===== как правильно получить ответ от сервера?
+};
+
+submitButton.addEventListener('click', onButtonClick);
+
+// ===================как из нижепривндннного когда получить true\false?
+loginForm.addEventListener(
+  'submit',
+  function () {
+    loginForm.reportValidity();
+  },
+  false,
+);
