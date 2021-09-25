8567e30c642e0fa490c65f8464978f75222f536a
Grimner1 <ursuld@gmail.com>
2021-09-25 22:04:43 +0300

    1

--- JS/lesson34/test3/index.js
+++ JS/lesson34/test3/index.js
@@ -1,13 +1,3 @@
-//  - по нажатию .submit-button - отправка данных формы на сервер:
-//      - собрать данные в объект из: id: email, name, password;
-//          или попробовать собрать с массива классов: .form-input
-//      - объект отправить на сервер;
-//      - объект вывести в алерт;
-//      - очистить поля форы;
-//  - Если произошло ошибка при создании пользователя - вывести
-//       'Failed to create user' в .error-text;
-//          - если было изменение в одном из полей - ошибку убрать;
-
 const loginForm = document.querySelector('.login-form');
 const inputArray = document.querySelectorAll('.form-input');
 const submitButton = document.querySelector('.submit-button');
@@ -28,50 +18,29 @@ const setInfo = (userInfo) =>
   });
 
 const clearinputFields = () => {
-  emailField.value = ''; // <=== тут очистка полей после ввода
+  emailField.value = '';
   nameField.value = '';
   passField.value = '';
 };
 
-let xxx = null;
-
 const onButtonClick = (event) => {
-  // ==== тут я отменяю дефолтное поведение сабмита - правильно?
   event.preventDefault();
 
-  // ==== тут проверка на пустые строки
-  if (!(emailField.value && nameField.value && passField.value)) {
-    errorField.innerHTML = 'Failed to create user';
-    return;
-  }
-
-  // === тут проверка на наличие @ в эмейле
-  if (!emailField.value.includes('@')) {
-    errorField.textContent = 'Failed to create user';
-    return;
-  }
-
-  // === тут я собираю инфо с полей в объект
   const userInfo = {
     email: emailField.value,
     name: nameField.value,
     password: passField.value,
   };
 
-  // clearinputFields();
-
   setInfo(userInfo)
-    .then((data) => data.json())
+    .then((data) => data.json)
     .then((data) => {
       alert(JSON.stringify(data));
       loginForm.reset();
-    }) // <=== тут я пытась принять ответ от сервера если отправка данных успешна
-    .catch(() => (errorField.textContent = 'Failed to create user')); // <=== тут я принимаю ошибки от сервера
+    })
+    .catch(() => (errorField.textContent = 'Failed to create user'));
 };
 
-submitButton.addEventListener('click', onButtonClick);
-
-// === тут я снимаю\задаю дизейбл кнопке в зависимости от валидности полей
 const checkValidForm = () => {
   if (loginForm.reportValidity()) {
     submitButton.removeAttribute('disabled', 'disabled');
@@ -80,11 +49,10 @@ const checkValidForm = () => {
   }
 };
 
-loginForm.addEventListener('input', checkValidForm);
-
-// === тут я очищаю после с ошибкой если были сделаны любые изменения с инпутами
 const cleanError = () => {
   errorField.innerHTML = '';
 };
 
+submitButton.addEventListener('click', onButtonClick);
+loginForm.addEventListener('input', checkValidForm);
 loginForm.addEventListener('input', cleanError);
