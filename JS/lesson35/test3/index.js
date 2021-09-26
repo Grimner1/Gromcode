cd7cc8c984f7b5c2efc6f17e591e99248169de04
Grimner1 <ursuld@gmail.com>
2021-09-27 00:44:05 +0300

    add l35 t3

---
+++ JS/lesson35/test3/index.js
@@ -0,0 +1,75 @@
+const userAvatar = document.querySelector('.user__avatar');
+const userName = document.querySelector('.user__name');
+const userLocation = document.querySelector('.user__location');
+const spinner = document.querySelector('.spinner');
+const repoList = document.querySelector('.repo-list');
+const inputForm = document.querySelector('.name-form__input');
+const button = document.querySelector('.name-form__btn');
+
+// по нажатию кнопки, считать текст с инпута и отправить запрос на сервер;
+//      - url: https://api.github.com/users/
+//      - запрос через фетч:
+//              - в полученном промисе нужны следующие поля:
+//                      - avatar_url, name, location, repos_url;
+//              - полученные данные рендерим на страницу;
+//              - поcле полученного промисе сразу делаем запрос на repos_url
+//              - в ответ получаем список. Выделяем после 'name' которые рендерим
+//                  в  repo-list; Каждый элемент li имеет класс '.repo-list__item'
+//              - в случае любой ошибки нужно словить её в catch и вывести alert:
+//                  'Failed to load data';
+//      - все файлы разбить на модули
+//      - спиннер запускается сразу после нажатия на кнопку и выключается
+//                  после всех событий в  finaly;
+
+const url = 'https://api.github.com/users/';
+
+userAvatar.src = 'https://avatars3.githubusercontent.com/u10001';
+
+const hideShinner = () => {
+  spinner.classList.remove('spinner_hidden');
+};
+
+const showSpinner = () => {
+  spinner.classList.add('spinner_hidden');
+};
+
+const renderList = (objList) => {
+  const listToRender = objList.map(({ name }) => {
+    const listElement = document.createElement('li');
+    listElement.classList.add('repo-list__item');
+    listElement.textContent = name;
+    return listElement;
+  });
+  repoList.append(...listToRender);
+};
+
+const getUserRepo = (userUrl) => {
+  fetch(userUrl)
+    .then((promis) => promis.json())
+    .then((userInfo) => JSON.stringify(userInfo))
+    .then((userInfoInString) => JSON.parse(userInfoInString))
+    .then((userInfoInObject) => renderList(userInfoInObject))
+    .catch(() => alert('Failed to load data'))
+    .finally(() => {
+      showSpinner();
+    });
+};
+
+const getUserInfo = () => {
+  hideShinner();
+  repoList.textContent = '';
+  const userUrl = inputForm.value;
+  return fetch(url + userUrl)
+    .then((response) => response.json())
+    .then((userData) => {
+      const { avatar_url, name, location, repos_url } = userData;
+      userAvatar.src = avatar_url;
+      userName.textContent = name;
+      userLocation.textContent = location;
+      return repos_url;
+    })
+    .then((repoUrl) => getUserRepo(repoUrl))
+    .catch(() => alert('Failed to load data'));
+};
+
+button.addEventListener('click', getUserInfo);
